f21d625a0fc63e6412f718ee5a360851b5206ea5
William Schwindt <williammschwindt@gmail.com>
2020-05-07 14:49:01 -0700

    score added, winning alerts user, reset game btn

--- WarCards/ViewController.swift
+++ WarCards/ViewController.swift
@@ -19,21 +19,51 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rightScoreLabel: UILabel!
     
+    @IBOutlet weak var winnerLabel: UILabel!
+    
+    @IBOutlet weak var resetBtn: UIButton!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resetBtn.layer.cornerRadius = 10
     }
     
+    @IBAction func resetGame(_ sender: Any) {
+        winnerLabel.text = "First One to 20 Wins"
+        leftScore = 0
+        leftScoreLabel.text = "\(leftScore)"
+        rightScore = 0
+        rightScoreLabel.text = "\(rightScore)"
+    }
     
     @IBAction func dealTapped(_ sender: Any) {
+        if leftScore < 20 && rightScore < 20 {
+            let leftNumber = Int.random(in: 2...14)
+            let rightNumber = Int.random(in: 2...14)
+            
+            leftImageView.image = UIImage(named: "card\(leftNumber)")
+            rightImageView.image = UIImage(named: "card\(rightNumber)")
+            
+            if leftNumber > rightNumber {
+                leftScore += 1
+                leftScoreLabel.text = "\(leftScore)"
+                if leftScore == 20 {
+                    winnerLabel.text = "Player 1 Wins!"
+                }
+            }
+            
+            else if leftNumber < rightNumber {
+                rightScore += 1
+                rightScoreLabel.text = "\(rightScore)"
+                if rightScore == 20 {
+                    winnerLabel.text = "CPU Wins!"
+                }
+            }
+        }
         
-        let leftNumber = Int.random(in: 2...14)
-        let rightNumber = Int.random(in: 2...14)
-        leftImageView.image = UIImage(named: "card\(leftNumber)")
-        
-        rightImageView.image = UIImage(named: "card\(rightNumber)")
     }
-    
-
 }
 
